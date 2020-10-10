a51c095e8ebec5fecea71d29d7f1a0edb61803bb
akotak <47652048+akotak@users.noreply.github.com>
2020-10-10 15:13:35 -0500

    Project 2 Alpha

---
+++ Assets/AnimationInterrupt.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class AnimationInterrupt : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject vbBtnObj;
+
+    AudioSource voiceline;
+    //bool buttonPressed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Interrupt");
+        animator.GetComponent<Animator>();
+        voiceline = GetComponent<AudioSource>();
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Button pressed");
+
+        //buttonPressed = true;
+        //animator.Play("stand");
+        animator.Play("sit idle");
+        //voiceline.Play();
+        /*if (buttonPressed && animator.GetCurrentAnimatorStateInfo(0).IsName("sit idle"))
+        {
+            voiceline.Play();
+        }*/
+
+        //animator.SetBool("buttonPressed", buttonPressed);
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //buttonPressed = false;
+        voiceline.Play();
+        animator.Play("stand");
+        //animator.Play("sit idle");
+        Debug.Log("Button released");
+
+    }
+    
+    // Update is called once per frame
+    /*void Update()
+    {
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            buttonPressed = true;
+        }
+        else
+        {
+            buttonPressed = false;
+        }
+        if(buttonPressed && animator.GetCurrentAnimatorStateInfo(0).IsName("sit idle"))
+        {
+
+            voiceline.Play();
+        } 
+
+        animator.SetBool("buttonPressed", buttonPressed);
+    }
+    */
+}
---
+++ Assets/click.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class click : MonoBehaviour
+{
+
+    public GameObject vbBtnObj;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Interrupt");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Button pressed");
+
+    
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+        Debug.Log("Button released");
+      
+    }
+}
